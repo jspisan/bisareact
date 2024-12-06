e19172004e53b1a2395135dc5f9f02fdda84a5c0
Andi Hidayat <andi.hidayat.mail@gmail.com>
2024-12-07 01:53:12 +0700

    tempale literal

---
+++ 3tempaleLiteral/index.js
@@ -0,0 +1,6 @@
+//Template literal = Untuk menggabubgkan string dg cara yg lebih mudah
+
+const firstname = "Andi";
+const lastname = "Hidayat";
+const fullname = `Nama saya  ${firstname} ${lastname}`;
+console.log(fullname);
