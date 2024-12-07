748c5564b100bdc33222aad52fb8843be36233e7
Andi Hidayat <andi.hidayat.mail@gmail.com>
2024-12-08 01:02:22 +0700

    ternary operator

---
+++ 9ternariesOp/index.js
@@ -0,0 +1,15 @@
+function checkWIn(isWin) {
+  if (isWin) {
+    console.log("You win");
+  } else {
+    console.log("You lose");
+  }
+}
+
+checkWIn();
+
+function checkLose(isLose) {
+ isLose ? console.log("You lose") : console.log("You win")
+}   
+
+checkLose();
