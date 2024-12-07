391c6219161237a81802648e981080f101dbbf48
Andi Hidayat <andi.hidayat.mail@gmail.com>
2024-12-07 13:10:49 +0700

    short hand property name

---
+++ 4shorthandPropertyName/index.js
@@ -0,0 +1,16 @@
+//shorthand property name
+//mempersingkat dalam membuat object
+//yang propertinya sama dengan nama variable
+
+
+const namaDepan = "Andi"
+const namaBelakang = "Hidayat"
+const gelar = "S.P"
+
+const namaLengkap = {
+    namaDepan,
+    namaBelakang,
+    gelar
+}
+
+console.log(namaLengkap)
